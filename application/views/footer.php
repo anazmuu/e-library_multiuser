094a01eed461727963f54717ae5a9c7ca8568069
anazmuu <anaz.muu@outlook.com>
2019-10-30 19:19:11 +0700

    e-library_perpus

---
+++ application/views/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="copyright">
+            Copyright &copy;2019, Di Buat Oleh <a href="#">Team RPL SMK AL-MADANI Garut</a>
+        </div>              
+    </div>
+</footer>
